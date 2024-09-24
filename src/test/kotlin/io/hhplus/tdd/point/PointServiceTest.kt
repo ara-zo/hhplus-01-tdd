2a4cf5239e743c98350469c68e6940fdb5d961c2
ara-zo <jo91kr@gmail.com>
2024-09-25 08:02:52 +0900

    :white_check_mark: test: 포인트 충전/이용 내역 조회 테스트 구현

--- src/test/kotlin/io/hhplus/tdd/point/PointServiceTest.kt
+++ src/test/kotlin/io/hhplus/tdd/point/PointServiceTest.kt
@@ -1,21 +1,29 @@
 package io.hhplus.tdd.point
 
+import io.hhplus.tdd.domain.PointHistory
+import io.hhplus.tdd.domain.TransactionType
 import io.hhplus.tdd.domain.UserPoint
+import io.hhplus.tdd.repository.PointHistoryRepository
 import io.hhplus.tdd.repository.UserPointRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.BDDMockito.given
+import org.mockito.BDDMockito.`when`
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
+
 
 @ExtendWith(MockitoExtension::class)
 class PointServiceTest {
 
     @Mock
-    private lateinit var pointRepository: UserPointRepository
+    private lateinit var userPointRepository: UserPointRepository
+
+    @Mock
+    private lateinit var pointHistoryRepository: PointHistoryRepository
 
     @InjectMocks
     private lateinit var pointService: PointService
@@ -25,7 +33,7 @@ class PointServiceTest {
     fun findPointById() {
         // given
         val id = 1L
-        given(pointRepository.findById(id)).willReturn(UserPoint(id, 0, System.currentTimeMillis()))
+        given(userPointRepository.findById(id)).willReturn(UserPoint(id, 0, System.currentTimeMillis()))
 
         // when
         val result = pointService.findPointById(id)
@@ -34,4 +42,27 @@ class PointServiceTest {
         assertThat(result.id).isEqualTo(id)
         assertThat(result.point).isZero()
     }
+
+    @Test
+    @DisplayName("포인트 충전/이용 내역을 조회한다.")
+    fun history() {
+        // given
+        val id = 1L
+
+        // when
+        val pointHistoryList = listOf(
+            PointHistory(1L, id, TransactionType.CHARGE, 100L, System.currentTimeMillis()),
+            PointHistory(2L, id, TransactionType.USE, 10L, System.currentTimeMillis()),
+            PointHistory(3L, id, TransactionType.USE, 10L, System.currentTimeMillis())
+        )
+        `when`(pointHistoryRepository.findAllById(id)).thenReturn(pointHistoryList)
+
+        val result = pointHistoryRepository.findAllById(id)
+
+        // then
+        assertThat(result).hasSize(3)
+        assertThat(result.map { it.id }).containsExactlyInAnyOrder(id)
+        assertThat(result.get(0).amount).isEqualTo(100L)
+    }
+
 }
